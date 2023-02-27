f7e7bc1a5c36b860850938374e1fb788d395d390
ashahmoh <ashahmoh28@gmail.com>
2023-02-27 18:15:40 +0000

    dfs func added

--- main.go
+++ main.go
@@ -14,13 +14,17 @@ type antFarm struct {
 }
 
 type room struct {
-	Name   string
-	Tunnel []*room
+	Name     string
+	path     []*room
+	visited  bool
+	occupied bool
+	Tunnel   []*room
 }
 
 var (
-	from string
-	to   string
+	from      string
+	to        string
+	pathSlice [][]*room
 )
 
 func main() {
@@ -28,8 +32,6 @@ func main() {
 	ourFarm := &antFarm{}
 
 	file := readFile(os.Args[1])
-
-	allAnts()
 
 	var therooms string
 	for _, v := range file[1:] {
@@ -51,7 +53,7 @@ func main() {
 			words := regexp.MustCompile(" ").Split(remdash, -1)
 			from = words[0]
 			to = words[1]
-			fmt.Println("from:", from, "	to:", to)
+			//fmt.Println("from:", from, "	to:", to)
 
 		}
 
@@ -59,19 +61,23 @@ func main() {
 
 	ourFarm.addTunnel(from, to)
 	ourFarm.showRooms()
+	//DFS(ourFarm.startRoom(), *ourFarm)
 
 }
 
 func (f *antFarm) showRooms() {
+	allAnts()
+
 	for _, val := range f.Rooms {
-		//print out the name for each room
-		fmt.Printf("\nRoom: %v", val.Name)
+
 		for _, val := range val.Tunnel {
+			//add condition to check it doesn't print same room twice
 			fmt.Printf(" %v", val.Name)
 		}
+		//print out the name for each room
+		fmt.Printf("\nRoom: %v", val.Name)
 	}
 	fmt.Println()
-	//allAnts()
 	f.startRoom()
 	f.endRoom()
 }
@@ -86,7 +92,6 @@ func contains(rList []*room, rname string) bool {
 }
 
 func (f *antFarm) getRoom(name string) *room {
-
 	for index, val := range f.Rooms {
 		if val.Name == from || val.Name == to {
 			return f.Rooms[index]
@@ -106,6 +111,13 @@ func (f *antFarm) addTunnel(from, to string) {
 	} else if contains(fromRoom.Tunnel, to) {
 		err := fmt.Errorf("tunnel already exists (%v --> %v)", from, to)
 		fmt.Println(err.Error())
+	} else if fromRoom == toRoom {
+		err := fmt.Errorf("cannot connect room to itself (%v --> %v)", from, to)
+		fmt.Println(err.Error())
+	} else if fromRoom.Name == f.endRoom().Name {
+		toRoom.Tunnel = append(toRoom.Tunnel, fromRoom)
+	} else if toRoom.Name == f.startRoom().Name {
+		toRoom.Tunnel = append(toRoom.Tunnel, fromRoom)
 	} else {
 		fromRoom.Tunnel = append(fromRoom.Tunnel, toRoom)
 	}
@@ -129,6 +141,10 @@ func readFile(fname string) []string {
 func allAnts() {
 	file := readFile(os.Args[1])
 	ant := file[0]
+	if file[0] <= "0" {
+		err := fmt.Errorf("invalid number of ants")
+		fmt.Println(err.Error())
+	}
 	theAnts, _ := strconv.Atoi(string(ant))
 	fmt.Println("\nNumber of Ants:", theAnts)
 	fmt.Println()
@@ -146,7 +162,7 @@ func (f *antFarm) startRoom() *room {
 			if strings.Contains(start, " ") {
 				words := strings.Fields(start)
 				start = words[0]
-				fmt.Printf("\nstart Room: %v\n", start)
+				fmt.Printf("\nStart: %v\n", start)
 			}
 
 		}
@@ -160,14 +176,13 @@ func (f *antFarm) endRoom() *room {
 
 	var end string
 	file := readFile(os.Args[1])
-
 	for i := range file[1:] {
 		if file[i] == "##end" {
 			end = file[i+1]
 			if strings.Contains(end, " ") {
 				words := strings.Fields(end)
 				end = words[0]
-				fmt.Printf("End Room: %v\n", end)
+				fmt.Printf("End: %v\n", end)
 			}
 
 		}
@@ -175,4 +190,36 @@ func (f *antFarm) endRoom() *room {
 	}
 	return f.getRoom(end)
 
+}
+
+func DFS(r *room, f antFarm) {
+	visitedList := []string{}
+	startRoom := f.startRoom()
+	endRoom := f.endRoom()
+	if r.Name != endRoom.Name {
+		r.visited = true
+		visitedList = append(visitedList, r.Name)
+		for _, neighbour := range r.Tunnel {
+			if !neighbour.visited {
+				neighbour.path = append(r.path, neighbour)
+				if contains(neighbour.path, endRoom.Name) {
+
+					pathSlice = append(pathSlice, neighbour.path)
+				}
+				visitedList = append(visitedList, neighbour.Name)
+				DFS(neighbour, antFarm{f.Rooms})
+			}
+		}
+	} else {
+		if len(startRoom.Tunnel) > 1 && !contains(startRoom.Tunnel, endRoom.Name) {
+			visitedList = append(visitedList, r.Name)
+			fmt.Printf("1stvisitedList %v", visitedList)
+			startRoom.Tunnel = f.startRoom().Tunnel[1:][:]
+			DFS(startRoom, antFarm{f.Rooms})
+		} else {
+			visitedList = append(visitedList, r.Name)
+			fmt.Printf("2ndvisitedList %v\n", visitedList)
+			fmt.Println("start", startRoom.Name, "end", endRoom.Name)
+		}
+	}
 }
